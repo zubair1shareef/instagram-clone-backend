043841394c2d6698acb87149a39402e296c82067
zubair1shareef <zubair1shareef@gmail.com>
2023-02-04 19:00:10 +0530

    code formated

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -1,23 +1,23 @@
-const jwt =require('jsonwebtoken')
-const {JWT_SECRET}=require("../keys")
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require("../keys")
 const mongoose = require('mongoose')
-const User=mongoose.model("Users")
-module.exports=(req,res,next)=>{
-    const {authorization}=req.headers
-    if(!authorization){
-        return res.status(401).json({error:"you must be logged in"})
+const User = mongoose.model("Users")
+module.exports = (req, res, next) => {
+    const { authorization } = req.headers
+    if (!authorization) {
+        return res.status(401).json({ error: "you must be logged in" })
     }
-    const token=authorization.replace("Bearer ","")
-    jwt.verify(token,JWT_SECRET,(err,payload)=>{
-        if(err){
-            return res.status(401).json({error:"you must be logged in"})
+    const token = authorization.replace("Bearer ", "")
+    jwt.verify(token, JWT_SECRET, (err, payload) => {
+        if (err) {
+            return res.status(401).json({ error: "you must be logged in" })
 
         }
-        const {_id}=payload
-        User.findById(_id).then(userdata=>{
-            req.user=userdata
+        const { _id } = payload
+        User.findById(_id).then(userdata => {
+            req.user = userdata
             next()
         })
-       
+
     })
 }
