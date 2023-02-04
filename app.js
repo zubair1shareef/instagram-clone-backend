5d80a31df7ea2a36a328499b5f29074d8057a157
zubair1shareef <zubair1shareef@gmail.com>
2023-02-04 19:27:56 +0530

    fds

--- app.js
+++ app.js
@@ -33,11 +33,8 @@ const middleware = (req, res, next) => {
 };
 app.use(middleware)
 
-//edited
-
 
 app.use(
-
   router.get("/", (req, res) => {
     //res.json("for creating post:/createpost(loginreq)\n view all post: /post")
     res.json({
